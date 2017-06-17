9ae5538aa5d4ad7daad5adaa08ac06147d9c65e6
zhanghanbin1990 <568028573@qq.com>
2017-06-17 16:26:28 +0800

    Comparator比较器

---
+++ src/main/java/Comparator/ComparatorTest1.java
@@ -0,0 +1,27 @@
+package Comparator;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by zhanghanbin on 2017/6/16.
+ */
+public class ComparatorTest1 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("nihao", "hello", "world", "welcome");
+        Collections.sort(list, Comparator.comparingInt(String::length));
+        System.out.println(list);
+        Collections.sort(list,Comparator.comparingInt(String::length).reversed());
+        System.out.println(list);
+        System.out.println("============");
+        //Collections.sort(list,Comparator.comparingInt(item->item.length()).reversed());//编译器无法推断出item的类型
+        Collections.sort(list,Comparator.comparingInt((String item)->item.length()).reversed());
+        System.out.println(list);
+
+
+
+    }
+}
